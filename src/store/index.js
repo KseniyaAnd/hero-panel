a603095927f403c072ce03fceceb2a4889b5b9d5
Kseniya Andrusenka <kandrusenka@ibagroup.eu>
2024-07-10 04:55:22 +0300

    Redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,37 +1,21 @@
 import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
-import heroes from "../reducers/heroes";
-import filters from "../reducers/filters";
+import {thunk} from 'redux-thunk';
+import heroes from '../reducers/heroes';
+import filters from '../reducers/filters';
 
 const stringMiddleware = () => (next) => (action) => {
-    if (typeof action === "string") {
+    if (typeof action === 'string') {
         return next({
             type: action
         })
     }
-    return next(action);
-}
-
-const enhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-
-    const oldDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        if (typeof action === "string") {
-            return oldDispatch({
-                type: action
-            })
-        }
-        return oldDispatch(action);
-    }
-    return store;
-}
+    return next(action)
+};
 
 const store = createStore(
     combineReducers({heroes, filters}),
-    compose(
-        applyMiddleware(stringMiddleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    compose(applyMiddleware(thunk, stringMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 export default store;
